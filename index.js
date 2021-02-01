fee6188b83bc07365a05255354d9be70d0ea01de
Aleix Badia <aleix.284@gmail.com>
2021-02-01 12:30:52 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+    console.log(this.items);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    let max = -Infinity;
+    if (this.length > 0) {
+      this.items.forEach((el) => {
+        if (el > max) max = el;
+      });
+      return max;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    let min = Infinity;
+    if (this.length > 0) {
+      this.items.forEach((el) => {
+        if (el < min) min = el;
+      });
+      return min;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+      );
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
